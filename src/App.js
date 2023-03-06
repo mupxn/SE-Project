41c14a903000b9df35d9bd044911073e8421149f
mupxn <noey.ppk123@gmail.com>
2023-03-06 22:40:17 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import { NavBar } from './Element/NavBar';
+
 function App() {
   return (
     <div className="App">
