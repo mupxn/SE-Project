aafeca3663b4a923c8eb3be1a09aecbfa14acb6e
s6404062630082 <s6404062630082@email.kmutnb.ac.th>
2023-03-11 22:11:39 +0700

    Update AlertOrder.js

--- src/Employees/AlertOrder.js
+++ src/Employees/AlertOrder.js
@@ -24,10 +24,10 @@ function AlertOrder() {
       <div align="center" >
         <h5 >Order</h5>
       </div>
-      <div className="d-grid gap-2">
-      <Button variant="light" size="lg">test</Button>
+      <div className="d-grid gap-2" >
+      <Button variant="light" size="lg" ><h6 align='left'>OrderNumber : 1212312121</h6><br/> Tayida Capybara<br/>Details<br/></Button>
     </div>
-
+    <br/>
     </>
     
   );
