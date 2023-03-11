26d733b9407335132225bd14f8b48f67c9ec08fa
s6404062630082 <s6404062630082@email.kmutnb.ac.th>
2023-03-12 00:47:16 +0700

    Update OrderEM.js

--- src/Employees/OrderEM.js
+++ src/Employees/OrderEM.js
@@ -14,10 +14,9 @@ function OrderEM() {
             <h3>Order</h3>
           </Navbar.Brand>
           <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end"></Navbar.Collapse>
-          <Nav className="me-auto">
+           <Nav className="justify-content-right">
             <Nav.Link href="AlertOrder" ><NotificationsIcon/></Nav.Link>
-            <Nav.Link href="History"><HistoryIcon/></Nav.Link>
+            <Nav.Link href="History" ><HistoryIcon/></Nav.Link>
             <Nav.Link href="LoginEM" ><LoginIcon/></Nav.Link>
             
           </Nav>
