7e96fde6379962c161e6fa15a65b876cb1db2d9b
s6404062630082 <s6404062630082@email.kmutnb.ac.th>
2023-03-11 15:59:49 +0700

    Create OrderDetails.js

---
+++ src/Employees/OrderDetails.js
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+
+
+function OrderDetails() {
+  return (
+    <>
+      <Navbar bg="light" sticky="top"expand="md" variant="light">
+        <Container>
+          <Navbar.Brand >
+            <h3>OrderDetails</h3>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end"></Navbar.Collapse>
+          <Nav className="me-auto">
+            <Nav.Link href="OrderEM">black</Nav.Link>
+            
+          </Nav>
+        </Container>
+      </Navbar>
+
+    </>
+    
+  );
+}
+
+export default OrderDetails;
