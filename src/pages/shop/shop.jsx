67f0bf7d8d64966b9832721b3c51932afcb25524
s6404062630082 <s6404062630082@email.kmutnb.ac.th>
2023-03-16 21:00:38 +0700

    Update shop.jsx

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import { PRODUCTS } from "../../products";
 import { Product } from "./product";
 import "./shop.css";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
 
 export const Shop = () => {
   return (
     <div className="shop">
+      <div className="navbar">
+      <div className="links">
+        <Link to="/shop"> Shop </Link>
+        <Link to="/contact"> Contact </Link>
+        <Link to="/cart">
+          <ShoppingCart size={32} />
+        </Link>
+      </div>
+    </div>
+
       <div className="shopTitle">
         <h1>Salad Shop</h1>
       </div>
