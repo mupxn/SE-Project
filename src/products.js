572e37dcc75e579d3e9397c07bae43c9b7db2dbf
dxrkfey <praewmmpp@gmail.com>
2023-03-20 01:41:46 +0700

    sss

--- src/products.js
+++ src/products.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import product1 from "./assets/products/p1.jpg";
 import product2 from "./assets/products/p2.jpg";
 import product3 from "./assets/products/p3.jpg";
@@ -7,16 +8,33 @@ import product6 from "./assets/products/p6.jpg";
 import product7 from "./assets/products/7.webp";
 import product8 from "./assets/products/8.webp";
 
+/*const handleSubmit = (event) => {
+  event.preventDefault();
+  const data = new FormData(event.currentTarget);
+  const jsonData = {
+    id: data.get('id'),
+  }
+
+  fetch("http://localhost:3333/menu", {
+  method: "POST", // or 'PUT'
+  headers: {
+  "Content-Type": "application/json",
+},
+body: JSON.stringify(jsonData),
+})
+};*/
+
 
 export const PRODUCTS = [
+
   {
-    id: 1,
+    id: '01M',
     productName: "สลัดผัก",
     price: 45.0,
     productImage: product1,
   },
   {
-    id: 2,
+    id: '02M',
     productName: "สลัดโรลปูอัด",
     price: 45.0,
     productImage: product2,
