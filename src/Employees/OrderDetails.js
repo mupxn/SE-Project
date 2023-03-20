adeced3bcb82cde9e75f0c3c3fbfba8b8e54ac74
s6404062630082 <s6404062630082@email.kmutnb.ac.th>
2023-03-20 23:57:23 +0700

    Update OrderDetails.js

--- src/Employees/OrderDetails.js
+++ src/Employees/OrderDetails.js
@@ -2,25 +2,53 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import Badge from 'react-bootstrap/Badge';
-
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useState } from "react";
+import Divider from '@mui/material/Divider';
+import Axios from "axios";
 
 function OrderDetails() {
+
+  const [order, setOrder] = useState([]);
+  
+  Axios.get("http://localhost:3333/order").then((response) => {
+    setOrder(response.data);
+  });
+
   return (
     <>
        <Navbar bg="dark" variant="dark" sticky="top"expand="md" >
         <Container>
           <Navbar.Brand >
-            <h3>OrderDetails</h3>
+          <Nav.Link href="OrderEM" > <ArrowBackIcon/> 
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          OrderDetails</Nav.Link>
+            
           </Navbar.Brand>
           <Navbar.Toggle />
-        <Nav className="justify-content-right">
-            <Nav.Link href="OrderEM">black</Nav.Link>
-            
-          </Nav>
+        
         </Container>
       </Navbar>
-
+      <br/>
+      
+      <div >
+            {order.map((val) => {
+              return (
+                <div>
+                  <p align='left'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;OrderNo: {val.orderNo}</p>
+                  <p>OrderID: {val.orderID}</p>
+                  <p>user_id : {val.user_id }</p>
+                  <p>menuID: {val.menuID}
+                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {val.quantity}</p>
+                  <p>TotalPrice: {val.TotalPrice}</p>
+                  
+                  <Divider size="5"></Divider>
+                  </div>
+                  
+                );
+                
+            })}
+          </div>
     </>
     
   );
