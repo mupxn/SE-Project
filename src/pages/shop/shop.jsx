5b53663fee531498287e70c71bc670f9642ec8b0
dxrkfey <praewmmpp@gmail.com>
2023-04-08 23:46:17 +0700

    .

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -3,8 +3,34 @@ import { PRODUCTS } from "../../products";
 import { Product } from "./product";
 import "./shop.css";
 import { Navbar } from "../../components/navbar";
+import { useEffect } from "react";
+
 
 export const Shop = () => {
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    fetch("http://localhost:3333/authen", {
+      method: "POST", // or 'PUT'
+      headers: {
+      "Content-Type": "application/json",
+      'Authorization': 'Bearer'+token
+    },
+    })
+    .then(response=> response.json())
+    .then(data => {
+      if(data.status === 'ok'){
+        alert('authen success')
+      }else{
+          alert('authen failed')
+          localStorage.removeItem('token')
+          window.location = '/login'
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  }, [])
+
   return (
     
     <div className="shop">
