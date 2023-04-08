4405cdf584563c89082f06202ee49b8cffd3f403
dxrkfey <praewmmpp@gmail.com>
2023-04-08 16:44:14 +0700

    ..

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Navbar } from "../../components/navbar";
-import RECIEPT from "./"
+import Axios from "axios";
+import axios from 'axios';
+//import RECIEPT from "./"
+// Get the file input element and the upload button
+
+
 export const Checkout = () => {
+  const [image , setImage] = useState('')
+  function handleImage(e) {
+    console.log(e.target.file)
+    setImage(e.target.files[0])
+  }
+
+  function handleApi(){
+    const formData = new formData()
+    formData.append('image',image)
+    axios.post('url' , formData).then((res) => {
+      console.log(res)
+    })
+  }
   return (
     <div>
       <Navbar/>
       <div>
         <h1>Please Scan QR Code</h1>
+        <input type="file" name='file' onChange={handleImage}/>
+        <button onClick={handleApi}>Submit</button>
       </div>
     </div>
+    
   )
 }
