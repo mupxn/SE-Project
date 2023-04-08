170b9305d909b6acf993a205705ab6ec67060be7
dxrkfey <praewmmpp@gmail.com>
2023-04-08 13:34:25 +0700

    ..

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -5,11 +5,68 @@ import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
 import { Navbar } from "../../components/navbar";
 import "./cart.css";
+import { Product } from "../shop/product";
+
+
+
 export const Cart = () => {
   const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
-
   const navigate = useNavigate();
+
+
+  function fetchOrder() {
+
+    const data = {
+      amount : "3",
+      TotalPrice : "4",
+      status	: "รอยืนยันคำสั่งซื้อ" ,
+      user_id	: "21" 
+    };
+
+
+    fetch('http://localhost:3333/order', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+  function fetchDetail() {
+
+    const data = {
+      menu_name	: "สลัดผัก",
+      price	: "45",
+      amount : "1",
+      menuID : "1",
+      orderID	 : "23"
+    };
+
+    
+    fetch('http://localhost:3333/order_detail', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
 
   return (
     <div className="cart">
@@ -25,12 +82,14 @@ export const Cart = () => {
         })}
       </div>
 
+
       {totalAmount > 0 ? (
         <div className="checkout">
           <p> Subtotal: ${totalAmount} </p>
           <button onClick={() => navigate("/shop")}> Continue Shopping </button>
-          <button
-            onClick={() => {
+          <button id="check" onClick={() => {
+              fetchOrder();
+              fetchDetail();
               checkout();
               navigate("/checkout");
             }}
@@ -45,3 +104,4 @@ export const Cart = () => {
     </div>
   );
 };
+
