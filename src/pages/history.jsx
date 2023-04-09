b0910f2c1a5a1cb5cb8b52ea4c997aca77fc6dd9
dxrkfey <praewmmpp@gmail.com>
2023-04-10 01:50:27 +0700

    s

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -1,10 +1,48 @@
 import React from "react";
 import { Navbar } from "../components/navbar";
+import Axios from "axios";
+import { useState } from "react";
+import Divider from '@mui/material/Divider';
+import { Chip } from '@mui/material';
+
 export const HistoryCus = () => {
+    const [orderhis] = useState([]);
+    const [orderfinish, setorderfinish] = useState([]);
+    const myStyle2 = {
+    fontFamily: "Sans-Serif",
+    padding : "40px"
+    };
+    const myStyle3 = {
+    fontFamily: "Sans-Serif",
+    fontSize :"40px"
+    };
+
+    Axios.get("http://localhost:3333/orderfinish").then((response) => {
+    orderhis(response.data);
+    });
+
   return (
     <div className="history">
       <Navbar/>
-      <h3>HISTORY</h3>
+      <h1>HISTORY</h1>
+      {orderhis.map((val) => {
+        return (
+          <div style={myStyle2} >
+            <br></br>
+            <text style={myStyle2} > OrderID  {val.orderID} </text>
+            <text style={myStyle2} >amount  {val.amount }</text>
+            <text style={myStyle2} >TotalPrice  {val.TotalPrice} bath</text>
+            <b><Chip color='primary' label={val.status}  ></Chip></b>
+            
+            <br></br><br></br>
+            <Divider size="10px"></Divider>
+
+
+            </div>
+            
+          );
+          
+      })}
     </div>
   );
 };
