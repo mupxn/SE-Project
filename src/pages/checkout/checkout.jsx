1f4b5be14f8992ed3a3dd1f6424f4315bcf9609e
mupxn <noey.ppk123@gmail.com>
2023-04-11 00:43:23 +0700

    changeparment

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -3,6 +3,7 @@ import { Navbar } from "../../components/navbar";
 import RECIEPT from "../../receipt/Owner.png"
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
+import "./checkout.css"
 
 export const Checkout = () => {
   const navigate = useNavigate();
@@ -13,7 +14,7 @@ export const Checkout = () => {
         <h1>Please Scan QR Code</h1>
         <img src={RECIEPT} />
       </div>
-      <button onClick={() => navigate("/history-customer")}> payment submit </button>
+      <button className="payment" onClick={() => navigate("/history-customer")}> payment submit </button>
     </div>
   )
 }
