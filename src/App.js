399b14e6c06446e8675aab9c9deb33d26315728b
s6404062630082 <s6404062630082@email.kmutnb.ac.th>
2023-04-11 22:29:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import Statistics from './Employees/Statistics';
 import AlertOrder from './Employees/AlertOrder';
 import History from './Employees/History';
 import SignUp from './Customer/signup';
-import OrderDetails from './Employees/OrderDetails';
 import LoginCus from './Customer/LoginCus';
 import ShopCus from './Customer/shop-page';
 import { HistoryCus } from './pages/history';
@@ -49,7 +48,7 @@ function App() {
       <Route path="/Statistics" element={<Statistics/>} />
       <Route path="/AlertOrder" element={<AlertOrder/>} />
       <Route path="/History" element={<History/>} />
-      <Route path="/OrderDetails" element={<OrderDetails/>} />
+      
     </Routes>
   </Router>
      <ShopContextProvider>
